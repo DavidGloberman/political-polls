c05b7914adc760357a5c238541c34542ee0f4027
DavidGloberman <dag235479@gmail.com>
2026-09-09 19:15:29 +0300

    Remove image export bridge Vite plugin

--- political-arena/vite.config.ts
+++ political-arena/vite.config.ts
@@ -2,16 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "image-export-bridge",
-      transformIndexHtml(html) {
-        return html.replace(
-          "</body>",
-          '    <script type="module" src="/src/exportBridge.ts"></script>\n  </body>',
-        );
-      },
-    },
-  ],
+  plugins: [react()],
 });
